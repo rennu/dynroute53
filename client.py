767a8480489ee0a7af906c7bac80e7221289f969
Sakari <renfld@gmail.com>
2019-01-19 13:35:18 +0200

    Optimize and and update only what's changed

--- client.py
+++ client.py
@@ -6,7 +6,6 @@ from botocore.exceptions import ClientError
 def createParser():
   parser = argparse.ArgumentParser(description = "Dynamic DNS Client for Route 53")
   parser._action_groups.pop()
-
   args = parser.add_argument_group("Available arguments")
   args.add_argument("--config",
     help = "Path to configuration file",
@@ -20,45 +19,57 @@ def loadConfig(path):
       fp = open(path, "r")
       config = yaml.load(fp)
       fp.close()
+      return config
     except:
-      print "Could not open or parse config file."
-      return False
-    return True
+      print "Could not read or parse config."
+      sys.exit(1)
   else:
-    return False
+    print "Could not find config file."
+    sys.exit(1)
 
 def getMyIp():
-  ip = urllib2.urlopen(config["IPSource"]).read().strip()
-  isOk = False
   try:
+    ip = urllib2.urlopen(config["IPSource"]).read().strip()
     socket.inet_aton(ip)
     return ip
   except:
-    print "Could not get your IP."
+    print "Could not get IP from {0} or failed to parse result.".format(config["IPSource"])
     sys.exit(1)
+
+def getZones(client):
+  try:
+    zonesList = client.list_hosted_zones()
+  except ClientError as e:
+    print "Could not list zones"
+    print e
+  zones = {}
+  for zone in zonesList["HostedZones"]:
+    zoneName = zone["Name"][0:len(zone["Name"]) - 1].lower()
+    zoneValue = zone["Id"]
+    zones[zoneName] = zoneValue
+  return zones
 
 config = {}
 parser = createParser()
 args = parser.parse_args()
+config = loadConfig(args.config)
 
-if loadConfig(args.config):
-  session = boto3.Session(profile_name = config["AWSProfileName"])
-  client = session.client("route53")
+session = boto3.Session(profile_name = config["AWSProfileName"])
+client = session.client("route53")
+myip = getMyIp()
+zones = getZones(client)
 
-  myip = getMyIp()
-
-  zonesList = client.list_hosted_zones()
-  zones = {}
-  for zone in zonesList["HostedZones"]:
-    zoneName = zone["Name"][0:len(zone["Name"]) - 1].lower()
-    zoneValue = zone["Id"].split("/")[2]
-    zones[zoneName] = zoneValue
-
-  if len(config["zones"]) > 0:
-    for zone in config["zones"]:
-      if zone["zone"] in zones:
-        changes = []
-        for hostname in zone["hostnames"]:
+if len(config["zones"]) > 0:
+  for zone in config["zones"]:
+    if zone["zone"] in zones:
+      changes = []
+      for hostname in zone["hostnames"]:
+        currentIp = ""
+        try:
+          currentIp = socket.gethostbyname(hostname)
+        except:
+          ""
+        if currentIp != myip:
           changes.append({
             "Action": "UPSERT",
             "ResourceRecordSet": {
@@ -70,18 +81,19 @@ if loadConfig(args.config):
               ]
             }
           })
+      if len(changes) > 0:
         try:
           client.change_resource_record_sets(
             HostedZoneId = zones[zone["zone"].lower()],
             ChangeBatch = { "Changes": changes }
           )
-          print "Updated zone {0} A records using IP {1} for records: {2}." . format(zone["zone"], myip, ', '.join(zone["hostnames"]))
+          print "Updated zone {0} A records for {1} using IP {2}." . format(zone["zone"], ', '.join(map(lambda c: c["ResourceRecordSet"]["Name"], changes)), myip)
         except ClientError as e:
           print "Could not update zone: {0}." . format(zone["zone"])
           print e
       else:
-        print "Could not find zone {0} in Route53".format(zone["zone"])
-  else:
-    print "Could not find any zone definitions."
+        print "No changes detected."
+    else:
+      print "Could not find zone {0} in Route53.".format(zone["zone"])
 else:
-  print "Could not load config."
+  print "Could not find any zone definitions."
